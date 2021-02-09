199c7709676b304ca7acf295cbb7b1d93b1c2bdc
komazsofi <komazsofi@gmail.com>
2021-02-09 20:56:35 +0100

    add prediction

--- code/LiDAR_SDM/lidar_sdm.R
+++ code/LiDAR_SDM/lidar_sdm.R
@@ -20,6 +20,7 @@ names(lidar) <- c("reed_prop","ppr","density1_2","density2_3","density0_1","fhd"
 # extract area of interest
 
 lidar2 <- crop(lidar, extent(studyarea))
+landcover_crop <- mask(lidar2,studyarea)
 
 # check collinearity
 
@@ -30,8 +31,11 @@ vifstep(lidar2,th=5)
 data_forsdm <- sdmData(formula=occurrence~reed_prop+ppr+density1_2+density2_3+density0_1+fhd+kurto+perc_25+height+vv_std, train=birds, predictors=lidar2)
 data_forsdm
 
-model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf'),replication=c('boot'),n=50)
+model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf','maxent','gam'),replication=c('boot'),n=50)
 
-rcurve(model,id=50:100)
-vi <- getVarImp(model,method="brt")
+rcurve(model,id=1:150)
+vi <- getVarImp(model,method=c('brt','rf','maxent'))
 plot(vi)
+
+p1 <- predict(model,newdata=landcover_crop,filename='',method=c('brt','rf','maxent'))
+writeRaster(p1,"GrW_sdm_predict.tif",overwrite=TRUE)
