34609a97ed91543532f0ab4456b0b229da32eebc
komazsofi <komazsofi@gmail.com>
2021-02-09 10:12:40 +0100

    humanobj commented out

--- code/LiDAR_SDM/mask_lidar.R
+++ code/LiDAR_SDM/mask_lidar.R
@@ -14,12 +14,14 @@ humanobjectfile="D:/Koma/_PhD/Offline/Chapter3/Data_Preprocess/input_formask/pow
 landcover=raster(landcoverfile)
 humanobject = readOGR(dsn=humanobjectfile)
 
-# process files
-
 filelist=list.files(pattern = "*.tif")
+
+# create masks
   
 lidar=stack(filelist[1])
 proj4string(lidar) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# landcover
 
 landcover_crop=crop(landcover,extent(lidar))
   
@@ -28,9 +30,30 @@ formask[landcover_crop==16 |landcover_crop==30 | landcover_crop==41 | landcover_
 proj4string(formask)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
   
 lgn7_mask_resampled=resample(formask,lidar)
+writeRaster(lgn7_mask_resampled,"lgn7_mask.tif",overwrite=TRUE)
+
+# powerline filter
+
+#humanobj_sel=crop(humanobject,extent(lidar))
+#humanobj_rast <- rasterize(humanobj_sel, lidar,field="hoogtenive")
+#humanobj_rast_resampled=resample(humanobj_rast,lidar)
+#writeRaster(humanobj_rast_resampled,"humanobj_mask.tif",overwrite=TRUE)
+#lidar_masked_2 <- mask(lidar_masked, humanobj_rast_resampled,maskvalue=0)
   
 # apply
-lidar_masked <- mask(lidar, lgn7_mask_resampled)
+
+for (i in filelist) {
+  print(i)
+  
+  lidar=stack(i)
+  proj4string(lidar) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+  
+  lidar_masked <- mask(lidar, lgn7_mask_resampled)
+  
+  getfilename=str_sub(i,1,-5)
+  
+  writeRaster(lidar_masked,paste(workingdirectory,"/masked/",getfilename,"_masked.tif",sep=""),overwrite=TRUE)
+}
   
 
 
