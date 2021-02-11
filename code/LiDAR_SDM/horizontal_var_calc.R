9c505b8cef4af28a5d5a1bed04f677150ecd30ef
komazsofi <komazsofi@gmail.com>
2021-02-11 15:53:48 +0100

    make hor var sd dsm

--- code/LiDAR_SDM/horizontal_var_calc.R
+++ code/LiDAR_SDM/horizontal_var_calc.R
@@ -15,15 +15,20 @@ height=raster(heightfile)
 # calculate horizontal metrics
 
 height_class_reed=reclassify(height, c(c(-Inf,1,0,1,3,1,3,5,0,5,Inf,0)))
+height_class_lowveg=reclassify(height, c(c(-Inf,5,1,5,Inf,0)))
+
+height_masked <- mask(height,height_class_lowveg,maskvalue=0)
 
 beginCluster(15)
 
 sd_dsm=clusterR(height, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TRUE,na.rm = TRUE))
-prop_reedveg=clusterR(height_class_reed, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
+sd_dsm_low=clusterR(height_masked, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TRUE,na.rm = TRUE))
+#prop_reedveg=clusterR(height_class_reed, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
 
 endCluster()
 
 # export
 
 writeRaster(sd_dsm,"ahn3_feat_10m_1m_all_TILE_000_BAND_sd_dsm.tif",overwrite=TRUE)
-writeRaster(prop_reedveg,"ahn3_feat_10m_1m_all_TILE_000_BAND_prop_reedveg.tif",overwrite=TRUE)
+writeRaster(sd_dsm_low,"ahn3_feat_10m_1m_all_TILE_000_BAND_sd_dsm_low.tif",overwrite=TRUE)
+#writeRaster(prop_reedveg,"ahn3_feat_10m_1m_all_TILE_000_BAND_prop_reedveg.tif",overwrite=TRUE)
