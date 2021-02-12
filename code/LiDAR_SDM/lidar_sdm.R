73ecab6b98ef4dc54137d1dc84381d07531c858c
komazsofi <komazsofi@gmail.com>
2021-02-12 10:57:49 +0100

    cleaning up sdm process

--- code/LiDAR_SDM/lidar_sdm.R
+++ code/LiDAR_SDM/lidar_sdm.R
@@ -15,29 +15,27 @@ studyarea = readOGR(dsn=studyareafile)
 
 filelist=list.files(pattern = "*.tif")
 lidar=stack(filelist)
-names(lidar) <- c("reed_prop","ppr","density1_2","density2_3","density0_1","fhd","kurto","perc_25","height","vv_std")
+names(lidar) <- c("HH_reedveg_prop","C_ppr","HH_sd_low","HH_sd","VD_1_2","VD_2_3","VD_0_1","VV_FHD","VV_kurto","VV_p25","VV_p95","VV_std")
 
 # extract area of interest
 
 lidar2 <- crop(lidar, extent(studyarea))
 landcover_crop <- mask(lidar2,studyarea)
 
-# check collinearity
+# check for collinearity
 
 vifstep(lidar2,th=5)
 
-# sdm
+# sdm modelling
 
-data_forsdm <- sdmData(formula=occurrence~reed_prop+ppr+density1_2+density2_3+density0_1+fhd+kurto+perc_25+height+vv_std, train=birds, predictors=lidar2)
+data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar)
 data_forsdm
 
-model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf','maxent'),replication=c('boot'),n=50)
+model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf','maxent'),replication=c('cv','boot'),cv.folds=10,n=50)
 
+p1 <- ensemble(model, newdata=landcover_crop, filename='',setting=list(method='weighted',stat='AUC'),mean=T)
+
+# interpretations
 rcurve(model,id=1:150)
 vi <- getVarImp(model,method=c('brt','rf','maxent'))
 plot(vi)
-
-#p1 <- predict(model,newdata=landcover_crop,filename='',method=c('brt','rf','maxent'))
-#writeRaster(p1,"GrW_sdm_predict.tif",overwrite=TRUE)
-
-p1 <- ensemble(model, newdata=landcover_crop, filename='',setting=list(method='weighted',stat='AUC'))
