c304c3ace455531d29b425afe30a909266f85d82
komazsofi <komazsofi@gmail.com>
2021-02-13 19:13:12 +0100

    small changes on prediction

--- code/LiDAR_SDM/lidar_sdm.R
+++ code/LiDAR_SDM/lidar_sdm.R
@@ -20,22 +20,41 @@ names(lidar) <- c("HH_reedveg_prop","C_ppr","HH_sd_low","HH_sd","VD_1_2","VD_2_3
 # extract area of interest
 
 lidar2 <- crop(lidar, extent(studyarea))
-landcover_crop <- mask(lidar2,studyarea)
+lidar_crop <- mask(lidar2,studyarea)
+writeRaster(lidar_crop,'lidar_crop.tif',overwrite=TRUE)
 
 # check for collinearity
 
 vifstep(lidar2,th=5)
 
-# sdm modelling
+# sdm modelling study area
 
-data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar)
+data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar_crop)
 data_forsdm
 
-model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf','maxent'),replication=c('cv','boot'),cv.folds=10,n=50)
+model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf','maxent'),replication=c('cv','boot'),cv.folds=5,n=10)
+#write.sdm(model,'ensemble_GRW_LiDAR_NL_cv10_boot_n50') 
 
-p1 <- ensemble(model, newdata=landcover_crop, filename='',setting=list(method='weighted',stat='AUC'),mean=T)
+p1 <- ensemble(model, newdata=lidar_crop, filename='',setting=list(method='weighted',stat='AUC'))
+p2 <- predict(model, newdata=lidar_crop, filename='',mean=T)
 
 # interpretations
-rcurve(model,id=1:150)
+rcurve(model)
 vi <- getVarImp(model,method=c('brt','rf','maxent'))
 plot(vi)
+
+# sdm modelling whole NL
+
+data_forsdm_nl <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar)
+data_forsdm_nl
+
+model_nl <- sdm(occurrence~.,data=data_forsdm_nl,methods=c('brt','rf','maxent'),replication=c('cv','boot'),cv.folds=5,n=10)
+#write.sdm(model,'ensemble_GRW_LiDAR_NL_cv10_boot_n50') 
+
+p_nl <- ensemble(model_nl, newdata=lidar_crop, filename='ensemble_GrW_NL.tif',setting=list(method='weighted',stat='AUC'),mean=T)
+p_nl2 <- predict(model_nl, newdata=lidar_crop, filename='model_GrW_NL.tif',mean=T)
+
+# interpretations
+rcurve(model_nl)
+vi_nl <- getVarImp(model_nl,method=c('brt','rf','maxent'))
+plot(vi_nl)
