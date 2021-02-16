9cff3b61a1679714f5a2710bf41f6045dddd871f
komazsofi <komazsofi@gmail.com>
2021-02-16 10:14:50 +0100

    small changes

--- code/LiDAR_SDM/lidar_sdm.R
+++ code/LiDAR_SDM/lidar_sdm.R
@@ -7,46 +7,46 @@ workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataproce
 setwd(workingdirectory)
 
 birdsfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/presabs.shp"
-studyareafile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/studyarea.shp"
 
 # Import
 birds = readOGR(dsn=birdsfile)
-studyarea = readOGR(dsn=studyareafile)
+studyarea = readOGR(dsn="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/studyarea.shp")
+studyarea2 = readOGR(dsn="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/smallstudyareas.shp")
 
 filelist=list.files(pattern = "*.tif")
 lidar=stack(filelist)
-names(lidar) <- c("HH_reedveg_prop","C_ppr","HH_sd_low","HH_sd","VD_1_2","VD_2_3","VD_0_1","VV_FHD","VV_kurto","VV_p25","VV_p95","VV_std")
+names(lidar) <- c("HH_reedveg_prop","C_ppr","HH_sd_low","HH_sd","VD_1_2","VD_2_3","VD_0_1","VV_FHD","VV_p25","VV_p95","VV_std")
 
 # extract area of interest
 
 lidar2 <- crop(lidar, extent(studyarea))
 lidar_crop <- mask(lidar2,studyarea)
-#writeRaster(lidar_crop,'lidar_crop.tif',overwrite=TRUE)
 
 # check for collinearity
 
-vifstep(lidar2,th=5)
+vif=vifstep(lidar_crop,th=5)
+lidar_selected=exclude(lidar_crop,vif)
 
 # sdm modelling study area
 
 data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar_crop)
 data_forsdm
 
-model <- sdm(occurrence~.,data=data_forsdm,methods=c('brt','rf','maxent'),replication=c('cv','boot'),cv.folds=5,n=5)
+model <- sdm(occurrence~.,data=data_forsdm,methods=c('rf'),replication=c('cv','boot'),cv.folds=5,n=5)
 model
-write.sdm(model,'ensemble_GRW_LiDAR_NL_cv5_boot_n5') 
+#write.sdm(model,'ensemble_GRW_LiDAR_NL_cv5_boot_n5') 
 
-#lidar_crop_df=as.data.frame(lidar_crop)
+# extract area of interest
 
-lidar_crop_m=rasterToPoints(lidar_crop)
-lidar_crop_df=as.data.frame(lidar_crop_m)
+lidar3 <- crop(lidar_crop, extent(studyarea2))
+lidar_crop2 <- mask(lidar3,studyarea2)
 
 p1 <- ensemble(model, newdata=lidar_crop_df[,-c(1,2)], filename='',setting=list(method='weighted',stat='AUC'))
-p2 <- predict(model, newdata=lidar_crop, filename='',mean=T)
+p2 <- predict(model, newdata=lidar_crop2, filename='',mean=T)
 
 # interpretations
 rcurve(model)
-vi <- getVarImp(model,method=c('brt','rf','maxent'))
+vi <- getVarImp(model,method=c('rf'))
 plot(vi)
 
 # sdm modelling whole NL
@@ -54,11 +54,13 @@ plot(vi)
 data_forsdm_nl <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar)
 data_forsdm_nl
 
-model_nl <- sdm(occurrence~.,data=data_forsdm_nl,methods=c('brt','rf','maxent'),replication=c('cv','boot'),cv.folds=5,n=10)
+model_nl <- sdm(occurrence~.,data=data_forsdm_nl,methods=c('rf'),replication=c('cv','boot'),cv.folds=5,n=5)
 #write.sdm(model,'ensemble_GRW_LiDAR_NL_cv10_boot_n50') 
 
 p_nl <- ensemble(model_nl, newdata=lidar_crop, filename='ensemble_GrW_NL.tif',setting=list(method='weighted',stat='AUC'),mean=T)
 p_nl2 <- predict(model_nl, newdata=lidar_crop, filename='model_GrW_NL.tif',mean=T)
+
+p2 <- predict(model_nl, newdata=lidar_crop2, filename='',mean=T)
 
 # interpretations
 rcurve(model_nl)
