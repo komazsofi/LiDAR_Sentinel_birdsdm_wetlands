5bdbef4a67d238ebdf4dea9f79b365b87336e7c5
komazsofi <komazsofi@gmail.com>
2021-02-16 12:19:16 +0100

    small adaptations

--- code/LiDAR_SDM/lidar_sdm.R
+++ code/LiDAR_SDM/lidar_sdm.R
@@ -6,7 +6,7 @@ library(usdm)
 workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/lidar/masked2/"
 setwd(workingdirectory)
 
-birdsfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/presabs.shp"
+birdsfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/presabs_800rand_studyarea.shp"
 
 # Import
 birds = readOGR(dsn=birdsfile)
@@ -29,40 +29,22 @@ lidar_selected=exclude(lidar_crop,vif)
 
 # sdm modelling study area
 
-data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar_crop)
+data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar_selected)
 data_forsdm
 
-model <- sdm(occurrence~.,data=data_forsdm,methods=c('rf'),replication=c('cv','boot'),cv.folds=5,n=5)
+model <- sdm(occurrence~.,data=data_forsdm,methods=c('rf'),replication=c('boot'),n=10)
 model
 #write.sdm(model,'ensemble_GRW_LiDAR_NL_cv5_boot_n5') 
 
 # extract area of interest
 
-lidar3 <- crop(lidar_crop, extent(studyarea2))
+lidar3 <- crop(lidar_selected, extent(studyarea2))
 lidar_crop2 <- mask(lidar3,studyarea2)
 
-p1 <- ensemble(model, newdata=lidar_crop_df[,-c(1,2)], filename='',setting=list(method='weighted',stat='AUC'))
+#p1 <- ensemble(model, newdata=lidar_crop_df[,-c(1,2)], filename='',setting=list(method='weighted',stat='AUC'))
 p2 <- predict(model, newdata=lidar_crop2, filename='',mean=T)
 
 # interpretations
 rcurve(model)
 vi <- getVarImp(model,method=c('rf'))
 plot(vi)
-
-# sdm modelling whole NL
-
-data_forsdm_nl <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=lidar)
-data_forsdm_nl
-
-model_nl <- sdm(occurrence~.,data=data_forsdm_nl,methods=c('rf'),replication=c('cv','boot'),cv.folds=5,n=5)
-#write.sdm(model,'ensemble_GRW_LiDAR_NL_cv10_boot_n50') 
-
-p_nl <- ensemble(model_nl, newdata=lidar_crop, filename='ensemble_GrW_NL.tif',setting=list(method='weighted',stat='AUC'),mean=T)
-p_nl2 <- predict(model_nl, newdata=lidar_crop, filename='model_GrW_NL.tif',mean=T)
-
-p2 <- predict(model_nl, newdata=lidar_crop2, filename='',mean=T)
-
-# interpretations
-rcurve(model_nl)
-vi_nl <- getVarImp(model_nl,method=c('brt','rf','maxent'))
-plot(vi_nl)
