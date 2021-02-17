5394982f24db10dd6e1a7d3fa6aca34c3f42414d
komazsofi <komazsofi@gmail.com>
2021-02-17 11:40:48 +0100

    fusion sdm

---
+++ code/LiDAR_SDM/fusion_sdm.R
@@ -0,0 +1,50 @@
+library(sdm)
+library(rgdal)
+library(raster)
+library(usdm)
+
+workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/both/"
+setwd(workingdirectory)
+
+birdsfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/presabs_800rand_studyarea.shp"
+
+# Import
+birds = readOGR(dsn=birdsfile)
+studyarea = readOGR(dsn="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/smallstudyareas.shp")
+
+filelist=list.files(pattern = "*.tif")
+radar=stack(filelist)
+
+lidar=stack("lidar.grd")
+
+radar2=resample(lidar,radar)
+
+rasters=stack(radar2,radar)
+
+# check for collinearity
+
+vif=vifstep(rasters,th=5)
+
+radar_selected=exclude(rasters,vif)
+radar_selected=rasters
+
+# sdm modelling study area
+
+data_forsdm <- sdmData(formula=occurrence~., train=birds[,-c(1,2)], predictors=radar_selected)
+data_forsdm
+
+model <- sdm(occurrence~.,data=data_forsdm,methods=c('rf'),replication=c('boot'),n=10)
+model
+#write.sdm(model,'ensemble_GRW_LiDAR_NL_cv5_boot_n5') 
+
+# extract area of interest
+
+radar2 <- crop(radar_selected, extent(studyarea))
+radar_crop <- mask(radar2,studyarea)
+
+p2 <- predict(model, newdata=radar_crop, filename='',mean=T)
+
+# interpretations
+rcurve(model)
+vi <- getVarImp(model,method=c('rf'))
+plot(vi)
