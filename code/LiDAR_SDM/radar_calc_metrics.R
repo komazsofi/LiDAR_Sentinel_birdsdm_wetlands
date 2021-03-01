f7919e2a36c00b0c9392ef7175846ecc98e4e09b
komazsofi <komazsofi@gmail.com>
2021-03-01 10:26:15 +0100

    adapt hor met calc

--- code/LiDAR_SDM/radar_calc_metrics.R
+++ code/LiDAR_SDM/radar_calc_metrics.R
@@ -5,25 +5,21 @@ library(ggplot2)
 library(snow)
 library(gridExtra)
 
-workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/radar2/merged/"
+workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/radar/merged/"
 setwd(workingdirectory)
-
-birdsfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/presabs_800rand_studyarea.shp"
 
 # Import
 
 filelist=list.files(pattern = "*.tif")
 radar=stack(filelist)
 
-birds = readOGR(dsn=birdsfile)
-
 # calc. horizontal metrics
 
-water_class=reclassify(radar[[1]], c(c(-Inf,-25,1,-25,Inf,0)))
+water_class=reclassify(radar[[11]], c(c(-Inf,-25,1,-25,Inf,0)))
 
-veg_masked_vh <- mask(radar[[1]],water_class,maskvalue=1)
-veg_masked_vv <- mask(radar[[3]],water_class,maskvalue=1)
-veg_masked_ndvi <- mask(radar[[7]],water_class,maskvalue=1)
+veg_masked_vh <- mask(radar[[11]],water_class,maskvalue=1)
+veg_masked_vv <- mask(radar[[15]],water_class,maskvalue=1)
+veg_masked_ndvi <- mask(radar[[3]],water_class,maskvalue=1)
 
 beginCluster(15)
 
@@ -32,9 +28,19 @@ sd_VV=clusterR(veg_masked_vv, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TR
 sd_ndvi=clusterR(veg_masked_ndvi, focal, args=list(w=matrix(1,11,11), fun=sd, pad=TRUE,na.rm = TRUE))
 prop_water=clusterR(water_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
 
+sd_VH_5=clusterR(veg_masked_vh, focal, args=list(w=matrix(1,5,5), fun=sd, pad=TRUE,na.rm = TRUE))
+sd_VV_5=clusterR(veg_masked_vv, focal, args=list(w=matrix(1,5,5), fun=sd, pad=TRUE,na.rm = TRUE))
+sd_ndvi_5=clusterR(veg_masked_ndvi, focal, args=list(w=matrix(1,5,5), fun=sd, pad=TRUE,na.rm = TRUE))
+prop_water_5=clusterR(water_class, focal, args=list(w=matrix(1,5,5), fun=sum, pad=TRUE,na.rm = TRUE))
+
 endCluster()
 
-writeRaster(sd_VH,'radar_metricsd_sd_VH_hor.tif',overwrite=TRUE)
-writeRaster(sd_VV,'radar_metricsd_sd_VV_hor.tif',overwrite=TRUE)
-writeRaster(sd_ndvi,'radar_metricsd_sd_ndvi_hor.tif',overwrite=TRUE)
-writeRaster(prop_water,'radar_metricprop_water.tif',overwrite=TRUE)
+writeRaster(sd_VH,'radar_VHsd_hor_100m.tif',overwrite=TRUE)
+writeRaster(sd_VV,'radar_VVsd_hor_100m.tif',overwrite=TRUE)
+writeRaster(sd_ndvi,'optical_NDVIsd_hor_100m.tif',overwrite=TRUE)
+writeRaster(prop_water,'radar_prop_water_100m.tif',overwrite=TRUE)
+
+writeRaster(sd_VH_5,'radar_VHsd_hor_50m.tif',overwrite=TRUE)
+writeRaster(sd_VV_5,'radar_VVsd_hor_50m.tif',overwrite=TRUE)
+writeRaster(sd_ndvi_5,'optical_NDVIsd_hor_50m.tif',overwrite=TRUE)
+writeRaster(prop_water_5,'radar_prop_water_50m.tif',overwrite=TRUE)
