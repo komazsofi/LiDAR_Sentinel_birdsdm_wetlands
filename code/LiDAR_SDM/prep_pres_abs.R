bffbecc970ed46dc2dd5b6d8f88186cb0086375e
komazsofi <komazsofi@gmail.com>
2021-03-02 10:31:41 +0100

    add additional regions

--- code/LiDAR_SDM/prep_pres_abs.R
+++ code/LiDAR_SDM/prep_pres_abs.R
@@ -13,7 +13,8 @@ birdsfile="Reedland_bird_observations.shp"
 ahn3_acqfile="ahn3_measuretime.shp"
 landcoverfile="LGN7.tif"
 #surveyfile="BMPplots_12530.shp"
-surveyfile="BMPplots_12380.shp"
+#surveyfile="BMPplots_12380.shp"
+surveyfile="BMPplots_13640.shp"
 
 # Import
 birds_sp = readOGR(dsn=birdsfile)
@@ -23,12 +24,13 @@ surveyplot = readOGR(dsn=surveyfile)
 
 # Filter presence according to the needs
 
-#birdsel_sp=birds_sp[(birds_sp@data$species=="Grote Karekiet" & birds_sp@data$year==2016),]
-birdsel_sp=birds_sp[(birds_sp@data$species=="Snor" & birds_sp@data$year==2016),]
+#birdsel_sp=birds_sp[(birds_sp@data$species=="Grote Karekiet"),]
+#birdsel_sp=birds_sp[(birds_sp@data$species=="Snor"),]
+birdsel_sp=birds_sp[(birds_sp@data$species=="Baardman"),]
 bird_ahn3ac=raster::intersect(birdsel_sp,ahn3_acq_sp)
 
-#bird_ahn3ac_filt=bird_ahn3ac[(bird_ahn3ac@data$year==bird_ahn3ac@data$Jaar),]
-bird_ahn3ac_filt=bird_ahn3ac[(bird_ahn3ac@data$OBJECTID==5),]
+bird_ahn3ac_filt=bird_ahn3ac[(bird_ahn3ac@data$year==bird_ahn3ac@data$Jaar),]
+bird_ahn3ac_filt2=bird_ahn3ac_filt[(bird_ahn3ac_filt@data$OBJECTID==5 | bird_ahn3ac_filt@data$OBJECTID==6 | bird_ahn3ac_filt@data$OBJECTID==11),]
 ahn3_acq_sp_filt=ahn3_acq_sp[(ahn3_acq_sp@data$OBJECTID==5 | ahn3_acq_sp@data$OBJECTID==6 | ahn3_acq_sp@data$OBJECTID==11),]
 
 surveyplot_filt=raster::intersect(surveyplot,ahn3_acq_sp_filt)
@@ -40,20 +42,20 @@ landcover_crop <- mask(landcover2, ahn3_acq_sp_filt)
 
 # select required landcover classes and create the mask
 
-bird_ahn3ac_filt_wlc=raster::extract(landcover_crop,bird_ahn3ac_filt)
-bird_ahn3ac_filt@data$landcover <- bird_ahn3ac_filt_wlc
+bird_ahn3ac_filt_wlc=raster::extract(landcover_crop,bird_ahn3ac_filt2)
+bird_ahn3ac_filt2@data$landcover <- bird_ahn3ac_filt_wlc
 
 formask <- setValues(raster(landcover_crop), NA)
 formask[landcover_crop==16 |landcover_crop==30 | landcover_crop==41 | landcover_crop==42 | landcover_crop==43 | landcover_crop==45] <- 1
 proj4string(formask)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 # where not to place absences
-bird_ahn3ac_filt_buff <- gBuffer(bird_ahn3ac_filt, width = 200)
+bird_ahn3ac_filt_buff <- gBuffer(bird_ahn3ac_filt2, width = 200)
 proj4string(bird_ahn3ac_filt_buff)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 
 # generate absences
-absence_cand=spsample(surveyplot_filt,n=2500,"random")
+absence_cand=spsample(surveyplot_filt,n=5000,"random")
 absence_cand.df=as.data.frame(absence_cand)
 absence_cand.df$occurrence <- 0
 
@@ -79,11 +81,13 @@ proj4string(absence)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763
 # create pres-abs file for SDM
 
 absence.df=as.data.frame(absence)
-presence.df=as.data.frame(bird_ahn3ac_filt)
+presence.df=as.data.frame(bird_ahn3ac_filt2)
 
 absence.dfsel=subset(absence.df,select=c(1,2,3))
 presence.dfsel=subset(presence.df,select=c(6,7,10))
 names(presence.dfsel)<- names(absence.dfsel)
+
+presence.dfsel=presence.dfsel[presence.dfsel$occurrence<2,]
 
 presabs=rbind(absence.dfsel,presence.dfsel)
 
@@ -93,7 +97,7 @@ coordinates(presabs)=~X_obs+Y_obs
 proj4string(presabs)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 # Export
-raster::shapefile(presabs,"presabs_Sn_rand_studyarea",overwrite=TRUE)
+raster::shapefile(presabs,"D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/both/presabs_Ba_rand_studyarea",overwrite=TRUE)
 
 #writeRaster(landcover_crop,'landcover_crop.tif',overwrite=TRUE)
 #writeRaster(formask,"wetland_mask.tif",overwrite=TRUE)
