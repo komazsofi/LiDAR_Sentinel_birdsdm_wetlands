3fe98eb9e5c2e661436db6a0d5827f2ecb70767d
Zsofia Koma <komazsofi@gmail.com>
2021-03-04 07:52:03 +0100

    spatial CV start to adapt for paper

--- code/LiDAR_SDM/rfsdm_wspatialcv.R
+++ code/LiDAR_SDM/rfsdm_wspatialcv.R
@@ -3,24 +3,40 @@ library(blockCV)
 library(raster)
 library(rgdal)
 
-library(randomForest)
-library(precrec)
-
 library(ggplot2)
 
 library(sdm)
+library(usdm)
 
-workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/both/"
+workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/both/"
 setwd(workingdirectory)
 
-birdsfile="D:/Sync/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/presabs_GrW_rand_studyarea.shp"
-
 # Import
-birds = readOGR(dsn=birdsfile)
-rasters=stack("lidar_sentinel.grd")
 
-proj4string(birds) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
-proj4string(rasters) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+filelist=list.files(pattern = "*.tif")
+all_predictor=stack(filelist)
+
+bird=readOGR(dsn="presabs_Ba_rand_studyarea.shp")
+
+proj4string(bird) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+proj4string(all_predictor) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+landcover=subset(all_predictor, c(1,2,3,4), drop=FALSE)
+lidar=subset(all_predictor, c(6,7,8,9,10,11,12,13,14,15,16), drop=FALSE)
+optical=subset(all_predictor, c(17,18,19,20,22,23,24,25,26), drop=FALSE)
+radar=subset(all_predictor, c(29,30,31,32,34,35,36,37,38,40), drop=FALSE)
+
+# VIF
+
+vif_lidar=vifstep(lidar,th=3)
+vif_optical=vifstep(optical,th=3)
+vif_radar=vifstep(radar,th=3)
+vif_landcover=vifstep(landcover,th=3)
+
+lidar_vif=exclude(lidar,vif_lidar)
+optical_vif=exclude(optical,vif_optical)
+radar_vif=exclude(radar,vif_radar)
+landcover_vif=exclude(landcover,vif_landcover)
 
 # intersect
 mydata <- raster::extract(rasters, birds, df = TRUE)
@@ -55,50 +71,7 @@ sb <- spatialBlock(speciesData = mydata_clean2,
                    xOffset = 0, # shift the blocks horizontally
                    yOffset = 0)
 
-# RF
 folds <- sb$folds
-
-testTable <- mydata_clean2
-testTable$pred <- NA
-
-for(k in seq_len(length(folds))){
-  trainSet <- unlist(folds[[k]][1]) # training set indices
-  testSet <- unlist(folds[[k]][2]) # testing set indices
-  
-  rf <- randomForest(x=mydata_clean[trainSet, c(1:8)], y=mydata_clean[trainSet, 17], 
-                      ntree=25, importance =T)
-  varImpPlot(rf)
-  testTable$pred[testSet] <- predict(rf, mydata_clean[testSet, c(1:8,17)], type='response') # predict the test set
-}
-
-precrec_obj <- evalmod(scores = testTable$pred, labels = testTable$occurrence)
-autoplot(precrec_obj)
-
-for(k in seq_len(length(folds))){
-  trainSet <- unlist(folds[[k]][1]) # training set indices
-  testSet <- unlist(folds[[k]][2]) # testing set indices
-  
-  rf <- randomForest(x=mydata_clean[trainSet, c(9:16)], y=mydata_clean[trainSet, 17], 
-                     ntree=25, importance =T)
-  varImpPlot(rf)
-  testTable$pred[testSet] <- predict(rf, mydata_clean[testSet, c(9:16,17)], type='response') # predict the test set
-}
-
-precrec_obj2 <- evalmod(scores = testTable$pred, labels = testTable$occurrence)
-autoplot(precrec_obj2)
-
-for(k in seq_len(length(folds))){
-  trainSet <- unlist(folds[[k]][1]) # training set indices
-  testSet <- unlist(folds[[k]][2]) # testing set indices
-  
-  rf <- randomForest(x=mydata_clean[trainSet, c(1:16)], y=mydata_clean[trainSet, 17], 
-                     ntree=25, importance =T)
-  varImpPlot(rf)
-  testTable$pred[testSet] <- predict(rf, mydata_clean[testSet, c(1:16,17)], type='response') # predict the test set
-}
-
-precrec_obj3 <- evalmod(scores = testTable$pred, labels = testTable$occurrence)
-autoplot(precrec_obj3)
 
 # with sdm package
 
@@ -114,7 +87,7 @@ for(k in seq_len(length(folds))){
   
   data_forsdm_lidar <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:8,17)],test=mydata_clean[testSet, c(1:8,17)])
   
-  model_lidar <- sdm(occurrence~.,data=data_forsdm_lidar,methods=c('rf'),replication=c('boot'),n=25)
+  model_lidar <- sdm(occurrence~.,data=data_forsdm_lidar,methods=c('rf'))
   print(model_lidar)
   
 }
