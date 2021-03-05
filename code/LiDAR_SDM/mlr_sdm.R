0d2ffc860f3c7d410db29f4fff08321c565f0570
Zsofia Koma <komazsofi@gmail.com>
2021-03-05 13:55:27 +0100

    accuracy assessment

--- code/LiDAR_SDM/mlr_sdm.R
+++ code/LiDAR_SDM/mlr_sdm.R
@@ -7,84 +7,111 @@ library(ggplot2)
 library(mmpf)
 library(iml)
 library(patchwork)
+library(sdm)
 
-workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/both/"
+workingdirectory="D:/Sync/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/both/"
 setwd(workingdirectory)
 
 # Import
 
-filelist=list.files(pattern = "*.tif")
-all_predictor=stack(filelist)
-
-bird=readOGR(dsn="presabs_GrW_rand_studyarea.shp")
-
-# Intersect
-mydata <- raster::extract(all_predictor, bird, df = TRUE)
-mydata$occurrence <- bird$occurrence
-mydata$x <- bird$x
-mydata$y <- bird$y
-
-mydata_clean=mydata[complete.cases(mydata), ]
-mydata_clean <- mydata_clean[,-1]
-
-# !!!!needs to be manually adjusted!!!!
-
-pres=mydata_clean[mydata_clean$occurrence==1,]
-abs=mydata_clean[mydata_clean$occurrence==0,]
-
-#pres_sampled=pres[sample(nrow(pres), 675), ]
-#presabs=rbind(pres_sampled,abs)
-
-abs_sampled=abs[sample(nrow(abs), 200), ]
-presabs=rbind(pres,abs_sampled)
-
+presabs=read.csv("presabs_Sn.csv")
 presabs$occurrence<-factor(presabs$occurrence)
+presabs=presabs[,-1]
 
 # start mlr
 
 set.seed(11)
 
-task = makeClassifTask(data = presabs[,c(1:19,20)], target = "occurrence",
+tasklid = makeClassifTask(data = presabs[,c(5:12,20)], target = "occurrence",
+                       positive = "1", coordinates = presabs[,c(21,22)])
+tasksent = makeClassifTask(data = presabs[,c(13:19,20)], target = "occurrence",
+                       positive = "1", coordinates = presabs[,c(21,22)])
+tasklandc = makeClassifTask(data = presabs[,c(1:4,20)], target = "occurrence",
+                       positive = "1", coordinates = presabs[,c(21,22)])
+tasklidsent = makeClassifTask(data = presabs[,c(5:19,20)], target = "occurrence",
+                       positive = "1", coordinates = presabs[,c(21,22)])
+taskall = makeClassifTask(data = presabs[,c(1:19,20)], target = "occurrence",
                        positive = "1", coordinates = presabs[,c(21,22)])
 
 lrnRF = makeLearner("classif.randomForest",
                     predict.type = "prob",
                     fix.factors.prediction = TRUE)
 
-perf_level_spCV = makeResampleDesc(method = "SpRepCV", folds = 3, reps = 25)
+perf_level_spCV = makeResampleDesc(method = "SpRepCV", folds = 4, reps = 25)
+#perf_level_spCV = makeResampleDesc(method = "RepCV", folds = 4, reps = 25)
 
-cvRF = resample(learner = lrnRF, task =task,
+# accuracy measure
+
+cvRF_lid = resample(learner = lrnRF, task =tasklid,
                 resampling = perf_level_spCV, 
-                measures = mlr::auc)
+                measures = list(auc,tpr,tnr))
 
-ctrl = makeTuneControlRandom(maxit = 10L)
+cvRF_sent = resample(learner = lrnRF, task =tasksent,
+                resampling = perf_level_spCV, 
+                measures = list(auc,tpr,tnr))
 
-paramsRF <- makeParamSet(
-  makeIntegerParam("mtry",lower = 1,upper = 8),
-  makeIntegerParam("nodesize",lower = 1,upper = 10)
-)
+cvRF_landc = resample(learner = lrnRF, task =tasklandc,
+                resampling = perf_level_spCV, 
+                measures = list(auc,tpr,tnr))
 
-tuneRF = tuneParams (learner = lrnRF, 
-                     task=task,
-                     resampling = perf_level_spCV,
-                     par.set = paramsRF,
-                     control = ctrl, 
-                     measures = mlr::auc,
-                     show.info = TRUE)
+cvRF_lidsent = resample(learner = lrnRF, task =tasklidsent,
+                resampling = perf_level_spCV, 
+                measures = list(auc,tpr,tnr))
 
-lrn = makeTuneWrapper(lrnRF, resampling = perf_level_spCV, par.set = paramsRF, control = ctrl)
+cvRF_all = resample(learner = lrnRF, task =taskall,
+                resampling = perf_level_spCV, 
+                measures = list(auc,tpr,tnr))
 
-model_all = train(lrn, task)
+acc_sprepcv_lid=cvRF_lid[["measures.test"]]
+acc_sprepcv_lid$tss=acc_sprepcv_lid$tpr+acc_sprepcv_lid$tnr-1
+acc_sprepcv_lid=acc_sprepcv_lid[complete.cases(acc_sprepcv_lid), ]
 
-# analyse
+acc_sprepcv_sent=cvRF_sent[["measures.test"]]
+acc_sprepcv_sent$tss=acc_sprepcv_sent$tpr+acc_sprepcv_sent$tnr-1
+acc_sprepcv_sent=acc_sprepcv_sent[complete.cases(acc_sprepcv_sent), ]
 
-predictor = Predictor$new(model_all, data = presabs[,c(1:19)], y = presabs$occurrence)
-imp = FeatureImp$new(predictor, loss = "ce")
-plot(imp)
+acc_sprepcv_landc=cvRF_landc[["measures.test"]]
+acc_sprepcv_landc$tss=acc_sprepcv_landc$tpr+acc_sprepcv_landc$tnr-1
+acc_sprepcv_landc=acc_sprepcv_landc[complete.cases(acc_sprepcv_landc), ]
 
-pdp.obj = Partial$new(predictor, feature = "optical_NDVIsd_hor_100m")
-plot(pdp.obj)
+acc_sprepcv_lidsent=cvRF_lidsent[["measures.test"]]
+acc_sprepcv_lidsent$tss=acc_sprepcv_lidsent$tpr+acc_sprepcv_lidsent$tnr-1
+acc_sprepcv_lidsent=acc_sprepcv_lidsent[complete.cases(acc_sprepcv_lidsent), ]
 
-effect = FeatureEffects$new(predictor,method="pdp")
-effect$plot(features = "optical_NDVIsd_hor_100m")
+acc_sprepcv_all=cvRF_all[["measures.test"]]
+acc_sprepcv_all$tss=acc_sprepcv_all$tpr+acc_sprepcv_all$tnr-1
+acc_sprepcv_all=acc_sprepcv_all[complete.cases(acc_sprepcv_all), ]
+
+print(paste("AUC lidar:",mean(acc_sprepcv_lid$auc),"+_",sd(acc_sprepcv_lid$auc)))
+print(paste("TSS lidar:",mean(acc_sprepcv_lid$tss),"+_",sd(acc_sprepcv_lid$tss)))
+
+print(paste("AUC sentinel:",mean(acc_sprepcv_sent$auc),"+_",sd(acc_sprepcv_sent$auc)))
+print(paste("TSS sentinel:",mean(acc_sprepcv_sent$tss),"+_",sd(acc_sprepcv_sent$tss)))
+
+print(paste("AUC landcover:",mean(acc_sprepcv_landc$auc),"+_",sd(acc_sprepcv_landc$auc)))
+print(paste("TSS landcover:",mean(acc_sprepcv_landc$tss),"+_",sd(acc_sprepcv_landc$tss)))
+
+print(paste("AUC lidarsentinel:",mean(acc_sprepcv_lidsent$auc),"+_",sd(acc_sprepcv_lidsent$auc)))
+print(paste("TSS lidarsentinel:",mean(acc_sprepcv_lidsent$tss),"+_",sd(acc_sprepcv_lidsent$tss)))
+
+print(paste("AUC all:",mean(acc_sprepcv_all$auc),"+_",sd(acc_sprepcv_all$auc)))
+print(paste("TSS all:",mean(acc_sprepcv_all$tss),"+_",sd(acc_sprepcv_all$tss)))
+
+# sdm
+
+data_forsdm_lidar <- sdmData(formula=occurrence~., train=presabs[, c(5:12,20)])
+model_lidar <- sdm(occurrence~.,data=data_forsdm_lidar,methods=c('rf'),replication=c('cv','boot'),cv.folds=4,n=25,test.percent=30)
+
+data_forsdm_sentinel <- sdmData(formula=occurrence~., train=presabs[, c(13:19,20)])
+model_sentinel <- sdm(occurrence~.,data=data_forsdm_sentinel,methods=c('rf'),replication=c('cv','boot'),cv.folds=4,n=25,test.percent=30)
+
+data_forsdm_landc <- sdmData(formula=occurrence~., train=presabs[, c(1:4,20)])
+model_landc <- sdm(occurrence~.,data=data_forsdm_landc,methods=c('rf'),replication=c('cv','boot'),cv.folds=4,n=25,test.percent=30)
+
+data_forsdm_lidsent <- sdmData(formula=occurrence~., train=presabs[, c(5:19,20)])
+model_lidsent <- sdm(occurrence~.,data=data_forsdm_lidsent,methods=c('rf'),replication=c('cv','boot'),cv.folds=4,n=25,test.percent=30)
+
+data_forsdm_lidall <- sdmData(formula=occurrence~., train=presabs[, c(1:19,20)])
+model_lidall <- sdm(occurrence~.,data=data_forsdm_lidall,methods=c('rf'),replication=c('cv','boot'),cv.folds=4,n=25,test.percent=30)
+
 
