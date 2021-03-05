a4db8cb3d46ea7b9ebc324880dedaf4696a25f0b
komazsofi <komazsofi@gmail.com>
2021-03-05 08:26:59 +0100

    adapt

--- code/LiDAR_SDM/rfsdm_wspatialcv.R
+++ code/LiDAR_SDM/rfsdm_wspatialcv.R
@@ -58,7 +58,7 @@ abs=mydata_clean[mydata_clean$occurrence==0,]
 pres_sampled=pres[sample(nrow(pres), 675), ]
 presabs=rbind(pres_sampled,abs)
 
-abs_sampled=abs[sample(nrow(abs), 150), ]
+abs_sampled=abs[sample(nrow(abs), 250), ]
 presabs=rbind(pres,abs_sampled)
 
 # create a shp file
@@ -80,8 +80,6 @@ sb <- spatialBlock(speciesData = mydata_clean2,
                    selection = "random",
                    iteration = 100, # find evenly dispersed folds
                    biomod2Format = TRUE,
-                   xOffset = 0, # shift the blocks horizontally
-                   yOffset = 0,
                    numLimit=80)
 
 folds <- sb$folds
@@ -90,52 +88,54 @@ folds <- sb$folds
 mydata_clean=presabs
 
 accuracy=data.frame(matrix(ncol = 9, nrow = 0))
-names(accuracy) <- c("AUC_lidar","AUC_radar","AUC_optical","AUC_sentinel","AUC_landcover",
-                     "AUC_lidrad","AUC_lidopt","AUC_lidsent","AUC_lidall") 
 
-feaimp=data.frame(matrix(ncol = 1, nrow = 18))
+feaimp=data.frame(matrix(ncol = 1, nrow = 19))
 
 
 for(k in seq_len(length(folds))){
   trainSet <- unlist(folds[[k]][1]) # training set indices
   testSet <- unlist(folds[[k]][2]) # testing set indices
   
-  data_forsdm_lidar <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:7,19)],test=mydata_clean[testSet, c(1:7,19)])
-  model_lidar <- sdm(occurrence~.,data=data_forsdm_lidar,methods=c('rf'))
+  data_forsdm_lidar <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:8,20)],test=mydata_clean[testSet, c(1:8,20)])
+  model_lidar <- sdm(occurrence~.,data=data_forsdm_lidar,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_radar <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(11:14,19)],test=mydata_clean[testSet, c(11:14,19)])
-  model_radar <- sdm(occurrence~.,data=data_forsdm_radar,methods=c('rf'))
+  data_forsdm_radar <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(12:15,20)],test=mydata_clean[testSet, c(12:15,20)])
+  model_radar <- sdm(occurrence~.,data=data_forsdm_radar,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_optical <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(8:10,19)],test=mydata_clean[testSet, c(8:10,19)])
-  model_optical <- sdm(occurrence~.,data=data_forsdm_optical,methods=c('rf'))
+  data_forsdm_optical <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(9:11,20)],test=mydata_clean[testSet, c(9:11,20)])
+  model_optical <- sdm(occurrence~.,data=data_forsdm_optical,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_sentinel <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(8:14,19)],test=mydata_clean[testSet, c(8:14,19)])
-  model_sentinel <- sdm(occurrence~.,data=data_forsdm_sentinel,methods=c('rf'))
+  data_forsdm_sentinel <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(9:15,20)],test=mydata_clean[testSet, c(9:15,20)])
+  model_sentinel <- sdm(occurrence~.,data=data_forsdm_sentinel,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_landc <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(15:18,19)],test=mydata_clean[testSet, c(15:18,19)])
-  model_landc <- sdm(occurrence~.,data=data_forsdm_landc,methods=c('rf'))
+  data_forsdm_landc <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(16:19,20)],test=mydata_clean[testSet, c(16:19,20)])
+  model_landc <- sdm(occurrence~.,data=data_forsdm_landc,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_lidrad <- sdmData(formula=occurrence~., train=mydata_clean[trainSet,c(1:7,11:14,19)],test=mydata_clean[testSet, c(1:7,11:14,19)])
-  model_lidrad <- sdm(occurrence~.,data=data_forsdm_lidrad,methods=c('rf'))
+  data_forsdm_lidrad <- sdmData(formula=occurrence~., train=mydata_clean[trainSet,c(1:8,11:19,20)],test=mydata_clean[testSet, c(1:8,11:19,20)])
+  model_lidrad <- sdm(occurrence~.,data=data_forsdm_lidrad,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_lidopt <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:7,8:10,19)],test=mydata_clean[testSet, c(1:7,8:10,19)])
-  model_lidopt <- sdm(occurrence~.,data=data_forsdm_lidopt,methods=c('rf'))
+  data_forsdm_lidopt <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:8,9:11,20)],test=mydata_clean[testSet, c(1:8,9:11,20)])
+  model_lidopt <- sdm(occurrence~.,data=data_forsdm_lidopt,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_lidsent <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:7,8:14,19)],test=mydata_clean[testSet, c(1:7,8:14,19)])
-  model_lidsent <- sdm(occurrence~.,data=data_forsdm_lidsent,methods=c('rf'))
+  data_forsdm_lidsent <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:8,9:15,20)],test=mydata_clean[testSet, c(1:8,9:15,20)])
+  model_lidsent <- sdm(occurrence~.,data=data_forsdm_lidsent,methods=c('rf'),replication=c('boot'),n=20)
   
-  data_forsdm_lidall <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:18,19)],test=mydata_clean[testSet,c(1:18,19)])
-  model_lidall <- sdm(occurrence~.,data=data_forsdm_lidall,methods=c('rf'))
+  data_forsdm_lidall <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(1:19,20)],test=mydata_clean[testSet,c(1:19,20)])
+  model_lidall <- sdm(occurrence~.,data=data_forsdm_lidall,methods=c('rf'),replication=c('boot'),n=20)
   
-  newline <- data.frame(t(c(model_lidar@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_radar@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_optical@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_sentinel@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_landc@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_lidrad@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_lidopt@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_lidsent@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC,
-                            model_lidall@models[["occurrence"]][["rf"]][["1"]]@evaluation[["test.indep"]]@statistics$AUC)))
+  model_lidar_acc=getEvaluation(model_lidar,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_radar_acc=getEvaluation(model_radar,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_optical_acc=getEvaluation(model_optical,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_sentinel_acc=getEvaluation(model_sentinel,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_landc_acc=getEvaluation(model_landc,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_lidrad_acc=getEvaluation(model_lidrad,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_lidopt_acc=getEvaluation(model_lidopt,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_lidsent_acc=getEvaluation(model_lidsent,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  model_lidall_acc=getEvaluation(model_lidall,stat=c('AUC','TSS','Deviance','Kappa'),opt=1)
+  
+  newline <- data.frame(t(c(mean(model_lidar_acc$AUC),mean(model_radar_acc$AUC),mean(model_optical_acc$AUC),
+                            mean(model_sentinel_acc$AUC),mean(model_landc_acc$AUC),mean(model_lidrad_acc$AUC),
+                            mean(model_lidopt_acc$AUC),mean(model_lidsent_acc$AUC),mean(model_lidall_acc$AUC))))
   
   accuracy <- rbind(accuracy, newline)
   
@@ -143,24 +143,12 @@ for(k in seq_len(length(folds))){
   feaimp=cbind(feaimp,newline2)
 }
 
-# feature importance visualization
-feaimp$matrix.ncol...1..nrow...18.<-model_lidall@models[["occurrence"]][["rf"]][["1"]]@varImportance[["training"]]@varImportance$variables
-forbarplot=data.frame(metrics=feaimp[,1], MeanAUCtest=rowMeans(feaimp[,-1]),lower=apply(feaimp[,-1], 1, FUN=min),upper=apply(feaimp[,-1], 1, FUN=max))
-
-forbarplot$color<-0
-forbarplot$color[1:8]<-1
-forbarplot$color[9:11]<-2
-forbarplot$color[12:15]<-3
-forbarplot$color[16:19]<-4
-
-ggplot(forbarplot, aes(x=metrics, y=MeanAUCtest,fill=as.factor(color))) + geom_bar(stat="identity", color="black", position=position_dodge())+
-  geom_errorbar(aes(ymin=lower, ymax=upper), width=.2,position=position_dodge(.9))+
-  coord_flip()+theme_bw(base_size = 12)+ylab("Feature Importance")+xlab("Metrics")+
-  scale_fill_manual(values = c("1" = "deeppink", "2" = "orange", "3" = "goldenrod4","4"="blue"),name="Metrics type",labels=c("LiDAR","Optical","RADAR","Landcover"))
+names(accuracy) <- c("AUC_lidar","AUC_radar","AUC_optical","AUC_sentinel","AUC_landcover",
+                     "AUC_lidrad","AUC_lidopt","AUC_lidsent","AUC_lidall") 
 
 # export
 write.csv(accuracy,"accuracy_GrW_blockcv_25km_5_30perc.csv")
-write.csv(forbarplot,"forbarplot_GrW_blockcv_25km_5_30perc.csv")
+write.csv(feaimp,"feaimp_GrW_blockcv_25km_5_30perc.csv")
 
 # whout blockCV
 
@@ -190,6 +178,3 @@ model_lidsent <- sdm(occurrence~.,data=data_forsdm_lidsent,methods=c('rf'),repli
 
 data_forsdm_lidall <- sdmData(formula=occurrence~., train=presabs[, c(1:18,19)])
 model_lidall <- sdm(occurrence~.,data=data_forsdm_lidall,methods=c('rf'),replication=c('boot'),n=25,test.percent=30)
-
-vi_lidall <- getVarImp(model_lidall,method=c('rf'))
-plot(vi_lidall)
