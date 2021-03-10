dedd5721590bbfc1436bd4848e62c60ab62f6e0e
komazsofi <komazsofi@gmail.com>
2021-03-10 21:25:55 +0100

    blockcv

---
+++ code/analysis2/sdm_wspatialcv.R
@@ -0,0 +1,85 @@
+library(blockCV)
+
+library(raster)
+library(rgdal)
+
+library(ggplot2)
+
+library(sdm)
+library(usdm)
+
+workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/both/"
+setwd(workingdirectory)
+
+# Import
+
+filelist=list.files(pattern = "*.tif")
+all_predictor=stack(filelist)
+
+#presabs=read.csv("presabs_Sn.csv")
+presabs=read.csv("presabs_GrW.csv")
+presabs=presabs[,-1]
+
+mydata_clean2=presabs
+
+coordinates(mydata_clean2)=~x+y
+proj4string(mydata_clean2)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
+
+# Spatial blocking
+
+sb <- spatialBlock(speciesData = mydata_clean2,
+                   species = "occurrence",
+                   rasterLayer = all_predictor,
+                   theRange = 25000, # size of the blocks
+                   k = 5,
+                   selection = "random",
+                   iteration = 100, # find evenly dispersed folds
+                   biomod2Format = TRUE,
+                   numLimit=80)
+
+folds <- sb$folds
+
+# with sdm package
+mydata_clean=presabs
+
+accuracy=data.frame(matrix(ncol = 5, nrow = 0))
+
+feaimp=data.frame(matrix(ncol = 0, nrow = 17))
+
+
+for(k in seq_len(length(folds))){
+  trainSet <- unlist(folds[[k]][1]) # training set indices
+  testSet <- unlist(folds[[k]][2]) # testing set indices
+  
+  data_forsdm_lidar <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(5:11,19)],test=mydata_clean[testSet, c(5:11,19)])
+  model_lidar <- sdm(occurrence~.,data=data_forsdm_lidar,methods=c('rf'),replication=c('boot'),n=5)
+  
+  data_forsdm_sentinel <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(12:18,19)],test=mydata_clean[testSet, c(12:18,19)])
+  model_sentinel <- sdm(occurrence~.,data=data_forsdm_sentinel,methods=c('rf'),replication=c('boot'),n=5)
+  
+  data_forsdm_landc <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(2:4,19)],test=mydata_clean[testSet, c(2:4,19)])
+  model_landc <- sdm(occurrence~.,data=data_forsdm_landc,methods=c('rf'),replication=c('boot'),n=5)
+  
+  data_forsdm_lidsent <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(5:18,19)],test=mydata_clean[testSet, c(5:18,19)])
+  model_lidsent <- sdm(occurrence~.,data=data_forsdm_lidsent,methods=c('rf'),replication=c('boot'),n=5)
+  
+  data_forsdm_lidall <- sdmData(formula=occurrence~., train=mydata_clean[trainSet, c(2:18,19)],test=mydata_clean[testSet,c(2:18,19)])
+  model_lidall <- sdm(occurrence~.,data=data_forsdm_lidall,methods=c('rf'),replication=c('boot'),n=5)
+  
+  model_lidar_acc=getEvaluation(model_lidar,stat=c('AUC','TSS','Deviance','Kappa'))
+  model_sentinel_acc=getEvaluation(model_sentinel,stat=c('AUC','TSS','Deviance','Kappa'))
+  model_landc_acc=getEvaluation(model_landc,stat=c('AUC','TSS','Deviance','Kappa'))
+  model_lidsent_acc=getEvaluation(model_lidsent,stat=c('AUC','TSS','Deviance','Kappa'))
+  model_lidall_acc=getEvaluation(model_lidall,stat=c('AUC','TSS','Deviance','Kappa'))
+  
+  newline <- data.frame(t(c(mean(model_lidar_acc$AUC),mean(model_sentinel_acc$AUC),mean(model_landc_acc$AUC),mean(model_lidsent_acc$AUC),
+                            mean(model_lidall_acc$AUC))))
+  
+  accuracy <- rbind(accuracy, newline)
+  
+  vi <- getVarImp(model_lidall, method='rf')
+  newline2=vi@varImportanceMean[["AUCtest"]]
+  
+  feaimp=cbind(feaimp,newline2)
+}
+
