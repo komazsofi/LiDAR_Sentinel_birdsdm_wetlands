4743f147b30c66852332e3d755e5387aab04a62f
komazsofi <komazsofi@gmail.com>
2021-04-20 11:40:19 +0200

    LGN8

--- code/LiDAR_SDM/mask_lidar.R
+++ code/LiDAR_SDM/mask_lidar.R
@@ -7,7 +7,7 @@ library(stringr)
 workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/lidar/"
 setwd(workingdirectory)
 
-landcoverfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/LGN7.tif"
+landcoverfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/2_Dataset/landcover_filter/landcover/UvA_LGN2018/LGN2018.tif"
 humanobjectfile="D:/Koma/_PhD/Offline/Chapter3/Data_Preprocess/input_formask/powerlines_buff20.shp"
 
 # Import
@@ -26,12 +26,12 @@ proj4string(lidar) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.387638
 landcover_crop=crop(landcover,extent(lidar))
   
 formask <- setValues(raster(landcover_crop), NA)
-formask[landcover_crop==16 |landcover_crop==30 | landcover_crop==41 | landcover_crop==42 | landcover_crop==43 | landcover_crop==45] <- 1
+formask[landcover_crop==16 |landcover_crop==30 | landcover_crop==41 | landcover_crop==42 | landcover_crop==43 | landcover_crop==45 | landcover_crop==322 | landcover_crop==332] <- 1
 proj4string(formask)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
   
 lgn7_mask_resampled=resample(formask,lidar)
-writeRaster(lgn7_mask_resampled,"lgn7_mask.tif",overwrite=TRUE)
-lgn7_mask_resampled=raster("lgn7_mask.tif")
+#writeRaster(lgn7_mask_resampled,"lgn7_mask.tif",overwrite=TRUE)
+#lgn7_mask_resampled=raster("lgn7_mask.tif")
 
 # powerline filter
 
@@ -54,8 +54,8 @@ for (i in filelist) {
   
   getfilename=str_sub(i,1,-5)
   
-  writeRaster(lidar_masked,paste(workingdirectory,"/masked/",getfilename,"_masked.tif",sep=""),overwrite=TRUE)
-  writeRaster(lidar_masked_2,paste(workingdirectory,"/masked2/",getfilename,"_masked_humanobj.tif",sep=""),overwrite=TRUE)
+  writeRaster(lidar_masked,paste(workingdirectory,"/masked3/",getfilename,"_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(lidar_masked_2,paste(workingdirectory,"/masked3/",getfilename,"_masked_humanobj.tif",sep=""),overwrite=TRUE)
 }
   
 
--- code/LiDAR_SDM/mask_radar.R
+++ code/LiDAR_SDM/mask_radar.R
@@ -7,7 +7,7 @@ library(stringr)
 workingdirectory="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/radar/merged/"
 setwd(workingdirectory)
 
-landcoverfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/LGN7.tif"
+landcoverfile="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/2_Dataset/landcover_filter/landcover/UvA_LGN2018/LGN2018.tif"
 
 # Import
 landcover=raster(landcoverfile)
@@ -24,7 +24,7 @@ proj4string(lidar) <- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.387638
 landcover_crop=crop(landcover,extent(lidar))
 
 formask <- setValues(raster(landcover_crop), NA)
-formask[landcover_crop==16 |landcover_crop==30 | landcover_crop==41 | landcover_crop==42 | landcover_crop==43 | landcover_crop==45] <- 1
+formask[landcover_crop==16 |landcover_crop==30 | landcover_crop==41 | landcover_crop==42 | landcover_crop==43 | landcover_crop==45 | landcover_crop==322 | landcover_crop==332] <- 1
 proj4string(formask)<- CRS("+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +no_defs")
 
 lgn7_mask_resampled=resample(formask,lidar)
@@ -43,5 +43,5 @@ for (i in filelist) {
   
   getfilename=str_sub(i,1,-5)
   
-  writeRaster(lidar_masked,paste(workingdirectory,"/masked/",getfilename,"_masked.tif",sep=""),overwrite=TRUE)
+  writeRaster(lidar_masked,paste(workingdirectory,"/masked2/",getfilename,"_masked.tif",sep=""),overwrite=TRUE)
 }
