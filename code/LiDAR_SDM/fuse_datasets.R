810b059356d3572698b3653b8e79ac0d5013e9ef
komazsofi <komazsofi@gmail.com>
2021-04-22 11:30:18 +0200

    add LGN8 and their metrics

--- code/LiDAR_SDM/fuse_datasets.R
+++ code/LiDAR_SDM/fuse_datasets.R
@@ -9,7 +9,7 @@ setwd(workingdirectory)
 
 # Import
 
-filelist=list.files(path="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/radar/merged/masked/",pattern = "*.tif",full.names = TRUE)
+filelist=list.files(path="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/radar/merged/masked2/",pattern = "*.tif",full.names = TRUE)
 sentinel=stack(filelist)
 
 filelist2=list.files(path="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper_march/lidar/",pattern = "*.tif",full.names = TRUE)
@@ -18,14 +18,14 @@ lidar=stack(filelist2)
 ahn3_acq_sp=readOGR(dsn="D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/ahn3_measuretime.shp")
 areaofinterest=ahn3_acq_sp[(ahn3_acq_sp@data$OBJECTID==5 | ahn3_acq_sp@data$OBJECTID==6 | ahn3_acq_sp@data$OBJECTID==11),]
 
-landcoverfile=stack("D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/3_Dataprocessing/dataprocess_forpaper/LGN7.tif")
+landcoverfile=stack("D:/Koma/Sync_PhD/_Amsterdam/_PhD/Chapter4_Sentinel/2_Dataset/landcover_filter/landcover/UvA_LGN2018/LGN2018.tif")
 
 # Crop lidar
 
 lidar2 <- crop(lidar, extent(areaofinterest))
 lidar_crop <- mask(lidar2, areaofinterest)
 
-names(lidar_crop) <- c("HH_reedveg_prop","C_ppr","HH_sd_low","HH_sd","VD_1_2","VD_2_3","VD_0_1","VV_FHD","VV_p25","VV_p95","VV_std")
+names(lidar_crop) <- c("lidar_HH_reedveg_prop","lidar_C_ppr","lidar_HH_sd_low","lidar_HH_sd","lidar_VD_1_2","lidar_VD_2_3","lidar_VD_0_1","lidar_VV_FHD","lidar_VV_kurt","lidar_VV_p25","lidar_VV_p95","lidar_VV_std")
 
 # Resampling
 sentinel2=resample(sentinel,lidar_crop)
@@ -50,20 +50,31 @@ landcover_resamp_mask <- mask(landcover_resamp,all_predictor[[1]],maskvalue=NA)
 reed_class=reclassify(landcover_resamp_mask, c(c(-Inf,41,0,42,42,1,43,Inf,0)))
 saltmarsh_class=reclassify(landcover_resamp_mask, c(c(-Inf,29,0,30,30,1,31,Inf,0)))
 swamp_class=reclassify(landcover_resamp_mask, c(c(-Inf,40,0,41,41,1,42,Inf,0)))
-water_class=reclassify(landcover_resamp_mask, c(c(-Inf,15,0,16,16,1,17,Inf,0)))
+
+forest_class=reclassify(landcover_resamp_mask, c(c(-Inf,42,0,43,43,1,44,Inf,0)))
+lowshrub_class=reclassify(landcover_resamp_mask, c(c(-Inf,321,0,322,322,1,323,Inf,0)))
+highshrub_class=reclassify(landcover_resamp_mask, c(c(-Inf,331,0,332,332,1,333,Inf,0)))
+
 
 beginCluster(15)
 
 landcover_propreed=clusterR(reed_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
 landcover_propsaltmarsh=clusterR(saltmarsh_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
 landcover_propswamp=clusterR(swamp_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
-landcover_propwater=clusterR(water_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
+
+landcover_propforest=clusterR(forest_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
+landcover_proplowshrub=clusterR(lowshrub_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
+landcover_prophighshrub=clusterR(highshrub_class, focal, args=list(w=matrix(1,11,11), fun=sum, pad=TRUE,na.rm = TRUE))
 
 endCluster()
 
-writeRaster(landcover_resamp_mask,'landcover_cat.tif',overwrite=TRUE)
+writeRaster(landcover_propsaltmarsh,'landcover_propsaltmarsh.tif',overwrite=TRUE)
 writeRaster(landcover_propreed,'landcover_propreed.tif',overwrite=TRUE)
 writeRaster(landcover_propswamp,'landcover_propswamp.tif',overwrite=TRUE)
-writeRaster(landcover_propsaltmarsh,'landcover_propsaltmarsh.tif',overwrite=TRUE)
+
+writeRaster(landcover_propforest,'landcover_propforest.tif',overwrite=TRUE)
+writeRaster(landcover_proplowshrub,'landcover_proplowshrub.tif',overwrite=TRUE)
+writeRaster(landcover_prophighshrub,'landcover_prophighshrub.tif',overwrite=TRUE)
+
 writeRaster(landcover_propwater,'landcover_propwater.tif',overwrite=TRUE)
 
